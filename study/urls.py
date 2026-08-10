17b182997155e095c9c861818d8642f361cdc641
YKNNagi <kyk.main@gmail.com>
2026-08-10 17:12:23 +0900

    Add user login and protected dashboard

--- study/urls.py
+++ study/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("",views.index, name="index"),
     path("signup/",views.signup, name="signup"),
+    path("login/",views.login_view,name="login"),
+    path("dashboard/", views.dashboard, name="dashboard"),
 ]
--- study/views.py
+++ study/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return HttpResponse("StudyStack")
@@ -9,10 +12,38 @@ def signup(request):
     if request.method == "POST":
 
         #登録ボタンを押した
-        pass
+        form =UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+        else:
+            print(form.errors)
+        
     else:
 
         #最初に登録画面を開いたとき
         form = UserCreationForm()
 
     return render(request, "study/signup.html", {"form": form})
+
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request,user)
+            return redirect("dashboard")
+      
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "study/login.html", {"form": form})
+
+@login_required(login_url="/login/")
+def dashboard(request):
+    return render(request, "study/dashboard.html")
