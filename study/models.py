9a13bb18753c648d95c85c89c5921e940049c60a
YKNNagi <kyk.main@gmail.com>
2026-08-10 15:11:26 +0900

    Add user signup form

--- study/models.py
+++ study/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Study(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete = models.CASCADE
+    )
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
--- study/views.py
+++ study/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 def index(request):
     return HttpResponse("StudyStack")
+
+def signup(request):
+    if request.method == "POST":
+
+        #登録ボタンを押した
+        pass
+    else:
+
+        #最初に登録画面を開いたとき
+        form = UserCreationForm()
+
+    return render(request, "study/signup.html", {"form": form})
