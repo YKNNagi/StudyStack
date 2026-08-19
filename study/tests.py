cf1348944699f439f1051b9ec2f35a7caed48e9a
YKNNagi <kyk.main@gmail.com>
2026-08-19 10:57:18 +0900

    Add StudyForm validation tests

--- study/tests.py
+++ study/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from django.test import TestCase
+from .forms import StudyForm
 
 # Create your tests here.
+
+class StudyFormTest(TestCase):
+
+    def test_valid_content(self):
+        form = StudyForm(
+            data={
+                "content": "Pythonを勉強する"
+            }
+        )
+
+        self.assertTrue(form.is_valid())
+
+
+    def test_short_content(self):
+            form = StudyForm(
+                data={
+                    "content": " a "
+                }
+            )
+    
+            self.assertFalse(form.is_valid())
+            self.assertIn("content", form.errors)
+
+    def test_whitespace_only_content(self):
+            form = StudyForm(
+                data={
+                    "content": "   "
+                }
+            )
+    
+            self.assertFalse(form.is_valid())
+            self.assertIn("content", form.errors)
+    
